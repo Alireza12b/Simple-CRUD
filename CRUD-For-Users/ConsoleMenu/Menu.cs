6dbd59ff8523e8cce0d3937ef18c49f6bca815a2
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-05-24 21:50:25 +0330

    added main menu

---
+++ CRUD-For-Users/ConsoleMenu/Menu.cs
@@ -0,0 +1,40 @@
+﻿using CRUD_For_Users.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_For_Users.ConsoleMenu
+{
+    public class Menu
+    {
+        IUserServices userServices = new UserServices();
+
+        public void MainMenu()
+        {
+            Console.WriteLine("Welcome !" +
+                "\nwhat do you want to do ?" +
+                "\n1-Add a User" +
+                "\n2-Show List of Users");
+
+            int.TryParse(Console.ReadLine(), out int mainMenuSelection);
+            if (mainMenuSelection == 1)
+            {
+                AddMenu();
+            }
+            else if (mainMenuSelection == 2)
+            {
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid selection . Please enter only number of menu item that you want !");
+                MainMenu();
+            }
+        }
+
+        
+    }
+}
--- CRUD-For-Users/Program.cs
+++ CRUD-For-Users/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace CRUD_For_Users
+﻿using CRUD_For_Users.ConsoleMenu;
+
+namespace CRUD_For_Users
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Menu menu = new Menu();
+            menu.MainMenu();
         }
     }
 }
