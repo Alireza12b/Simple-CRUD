5f8b27221841f6561b7bf150041a9c9f5c1c5928
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-05-24 12:48:43 +0330

    Add project files.

---
+++ CRUD-For-Users/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_For_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
