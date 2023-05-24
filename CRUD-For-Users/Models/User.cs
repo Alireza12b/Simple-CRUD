5c1bfe63572df2bf23ef0cfa916f1bb0a9b52d22
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-05-24 17:34:54 +0330

    Model of user added

---
+++ CRUD-For-Users/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_For_Users.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Fname { get; set; }
+        public int Phone { get; set; }
+        DateTime DateOfBirth { get; set; }
+    }
+}
