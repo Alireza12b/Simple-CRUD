9a8927644882cde0b3c6181e4ebe933efa5a184e
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-05-24 18:39:38 +0330

    deserializing the json file added to constructor for preventing of rewriting it

--- CRUD-For-Users/Services/UserServices.cs
+++ CRUD-For-Users/Services/UserServices.cs
@@ -1,9 +1,11 @@
 ﻿using CRUD_For_Users.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CRUD_For_Users.Services
@@ -19,7 +21,8 @@ namespace CRUD_For_Users.Services
             string currentDirectory = Directory.GetCurrentDirectory();
             mainPath = Path.Combine(currentDirectory, jsonPath);
 
-          
+            string allText = File.ReadAllText(mainPath);
+            user = JsonConvert.DeserializeObject<List<User>>(allText);
         }
         
 
